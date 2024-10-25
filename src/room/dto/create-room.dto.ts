eb64766689579b37ce5ceb5d6ea65999ccca978b
Igor Liakh <igorlyah060@gmail.com>
2024-10-25 19:23:07 +0300

    added validation to create room dto

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateRoomDto {
+  @IsString({ message: 'Поле должно быть строкой!' })
+  @IsNotEmpty({ message: 'Поле не может быть пустым!' })
   name: string;
 }
