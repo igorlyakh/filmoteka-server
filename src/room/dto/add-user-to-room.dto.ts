9372e6c872c84ab27d14077314b1828ce106d486
Igor Liakh <igorlyah060@gmail.com>
2024-10-25 20:20:12 +0300

    added validation messages

--- src/room/dto/add-user-to-room.dto.ts
+++ src/room/dto/add-user-to-room.dto.ts
@@ -1,8 +1,8 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AddUserToRoomDto {
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
+  @IsString({ message: 'Поле должно быть строкой!' })
+  @IsNotEmpty({ message: 'Поле не должно быть пустым!' })
+  @IsEmail({}, { message: 'Введите корректный email!' })
   email: string;
 }
