ce891b25b2fc2e5fe926bb2efb4bc69922abb986
Igor Liakh <igorlyah060@gmail.com>
2024-10-25 19:38:50 +0300

    added swagger docs for room

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateRoomDto {
+  @ApiProperty({
+    description: 'Название комнаты.',
+    required: true,
+    example: 'Комната друзей',
+  })
   @IsString({ message: 'Поле должно быть строкой!' })
   @IsNotEmpty({ message: 'Поле не может быть пустым!' })
   name: string;
--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,15 +1,34 @@
 import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { User as UserType } from '@prisma/client';
 import { User } from 'src/decorators/user.decorator';
 import { JwtAccessGuard } from 'src/guards/jwt-access.guard';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { RoomService } from './room.service';
 
+@ApiTags('Комнаты')
 @UseGuards(JwtAccessGuard)
 @Controller('room')
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
+  //! --------------------------- { Создание комнаты } ----------------------------------
+
+  @ApiOperation({ description: 'Создание новой комнаты', summary: 'создание комнаты' })
+  @ApiResponse({ status: 201, description: 'Комната успешно создана.' })
+  @ApiResponse({ status: 401, description: 'Пользователь не авторизован.' })
+  @ApiResponse({
+    status: 400,
+    description: 'Отправлены не корректные данные для создание комнаты.',
+  })
+  @ApiBody({ type: CreateRoomDto, description: 'Модель для создание комнаты:' })
+  @ApiBearerAuth()
   @HttpCode(201)
   @Post()
   async createRoom(@Body() dto: CreateRoomDto, @User() user: UserType) {
