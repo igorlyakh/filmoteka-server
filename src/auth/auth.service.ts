46f661d1ec37bd368b03a2d89c75e4f9a7be469c
Igor Liakh <igorlyah060@gmail.com>
2024-10-26 19:30:41 +0300

    updated auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,10 +28,15 @@ export class AuthService {
 
     const hashedPassword = await bcrypt.hash(dto.password, 10);
 
-    const user = await this.prisma.user.create({
-      data: { ...dto, password: hashedPassword },
-      include: { rooms: true, movie: true },
-    });
+    // const user = await this.prisma.user.create({
+    //   data: { ...dto, password: hashedPassword },
+    //   include: { rooms: true, movie: true },
+    // });
+
+    const user = await this.userService.createUser(
+      { email: dto.email, name: dto.name },
+      hashedPassword
+    );
 
     const { accessToken, refreshToken } = await this.generateToken(user.id, user.email);
 
