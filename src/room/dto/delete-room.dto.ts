966b7edc65d3c9e95279afc27bcaf6371312d1dc
Igor Liakh <igorlyah060@gmail.com>
2024-11-09 19:18:03 +0200

    add swagger docs for dtos

--- src/room/dto/delete-room.dto.ts
+++ src/room/dto/delete-room.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class DeleteRoomDto {
+  @ApiProperty({
+    description: 'ID комнаты.',
+    required: true,
+    example: 2,
+  })
   @IsNotEmpty({ message: 'Это обязательное поле!' })
   @IsNumber({}, { message: 'Значение должно быть числом!' })
   roomId: number;
--- src/room/dto/kick-user.dto.ts
+++ src/room/dto/kick-user.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class KickUserDto {
+  @ApiProperty({
+    description: 'ID пользователя.',
+    required: true,
+    example: 1,
+  })
   @IsNotEmpty({ message: 'Это обязательное поле!' })
   @IsNumber({}, { message: 'Значение должно быть числом!' })
   id: number;
