f4e10063c012d1ecf3c839306b88757e277d97ed
Igor Liakh <igorlyah060@gmail.com>
2024-11-09 20:00:58 +0200

    added gateway for room

---
+++ src/room/room.gateway.ts
@@ -0,0 +1,27 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    const userId = client.handshake.query.userId;
+    console.log('Client connected:', userId);
+  }
+
+  handleDisconnect(client: Socket) {
+    const userId = client.handshake.query.userId;
+    console.log('Client disconnected:', userId);
+  }
+}
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { RoomController } from './room.controller';
+import { RoomGateway } from './room.gateway';
 import { RoomService } from './room.service';
 
 @Module({
   controllers: [RoomController],
-  providers: [RoomService],
+  providers: [RoomService, RoomGateway],
   imports: [UserModule],
 })
 export class RoomModule {}
