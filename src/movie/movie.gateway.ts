94f623b2ea3a87783af2779d37ec968aea6428ae
Igor Liakh <igorlyah060@gmail.com>
2024-11-15 20:11:06 +0200

    update movie gateway structure

--- src/movie/movie.gateway.ts
+++ src/movie/movie.gateway.ts
@@ -1,28 +1,10 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { CreateMovieDto } from './dto/create-movie.dto';
-import { MovieService } from './movie.service';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class MovieGateway {
-  constructor(private readonly movieService: MovieService) {}
+  constructor() {}
 
-  @SubscribeMessage('createMovie')
-  create(@MessageBody() createMovieDto: CreateMovieDto) {
-    return this.movieService.create(createMovieDto);
-  }
-
-  @SubscribeMessage('findAllMovie')
-  findAll() {
-    return this.movieService.findAll();
-  }
-
-  @SubscribeMessage('findOneMovie')
-  findOne(@MessageBody() id: number) {
-    return this.movieService.findOne(id);
-  }
-
-  @SubscribeMessage('removeMovie')
-  remove(@MessageBody() id: number) {
-    return this.movieService.remove(id);
-  }
+  @WebSocketServer()
+  server: Server;
 }
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MovieService } from './movie.service';
+import { JwtModule } from '@nestjs/jwt';
 import { MovieGateway } from './movie.gateway';
+import { MovieService } from './movie.service';
 
 @Module({
   providers: [MovieGateway, MovieService],
+  imports: [JwtModule],
 })
 export class MovieModule {}
