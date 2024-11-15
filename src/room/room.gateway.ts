5fa1acdb66e695af027b0228b38ca703186d405a
Igor Liakh <igorlyah060@gmail.com>
2024-11-15 20:17:51 +0200

    remove connection from room gateway

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -1,57 +1,15 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
-import {
-  OnGatewayConnection,
-  OnGatewayDisconnect,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Room } from '@prisma/client';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService
-  ) {}
-
+export class RoomGateway {
   @WebSocketServer()
   server: Server;
-
-  handleConnection(client: Socket) {
-    const token = client.handshake.headers.authorization?.split(' ')[1];
-    if (token) {
-      try {
-        const user = this.jwtService.verify(token, {
-          secret: this.configService.getOrThrow('JWT_ACCESS_SECRET'),
-        });
-        console.log(`Connected user id: ${user.id}`);
-        client.join(user.id.toString());
-      } catch (error) {
-        console.log('err');
-      }
-    }
-  }
-
-  handleDisconnect(client: Socket) {
-    const token = client.handshake.headers.authorization?.split(' ')[1];
-    if (token) {
-      try {
-        const user = this.jwtService.verify(token, {
-          secret: this.configService.getOrThrow('JWT_ACCESS_SECRET'),
-        });
-        console.log(`Disconnected user id: ${user.id}`);
-        client.leave(user.id.toString());
-      } catch (error) {
-        console.log('err');
-      }
-    }
-  }
 
   addUserToNewRoom(userId: string, room: Room) {
     return this.server.to(userId).emit('addToRoom', room);
