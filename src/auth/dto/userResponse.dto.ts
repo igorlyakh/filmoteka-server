c74c7223c7ce645af944b81e2ad1e4ff5d961628
Igor Liakh <igorlyah060@gmail.com>
2024-11-25 19:44:48 +0200

    fix docs

--- src/auth/dto/userResponse.dto.ts
+++ src/auth/dto/userResponse.dto.ts
@@ -2,18 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class UserResponse {
   @ApiProperty({
-    description: 'Уникальный ID пользователя.',
-    example: '12',
-  })
-  id: number;
-
-  @ApiProperty({
-    description: 'Email пользователя.',
-    example: 'example@mail.com',
-  })
-  email: string;
-
-  @ApiProperty({
     description: 'Имя пользователя.',
     example: 'John',
     minLength: 3,
@@ -22,24 +10,10 @@ export class UserResponse {
   name: string;
 
   @ApiProperty({
-    description: 'Пароль пользователя.',
-    example: 'Sa3kljkl32111',
-    minLength: 6,
-    maxLength: 18,
+    description: 'Email пользователя.',
+    example: 'example@mail.com',
   })
-  password: string;
-
-  @ApiProperty({
-    description: 'Список комнат пользователя.',
-    example: '[23]',
-  })
-  rooms: string[];
-
-  @ApiProperty({
-    description: 'Список фильмов пользователя.',
-    example: ['Убить Билла'],
-  })
-  movie: string[];
+  email: string;
 
   @ApiProperty({
     description: 'Уникальный токен доступа пользователя.',
