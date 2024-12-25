a96fd535a5c873ad7cbe0c307e89e1441e6deffc
Igor Liakh <igorlyah060@gmail.com>
2024-12-25 18:25:22 +0200

    update loggin for sockets

--- src/connection.gateway.ts
+++ src/connection.gateway.ts
@@ -27,7 +27,7 @@ export class ConnectionGateway implements OnGatewayConnection, OnGatewayDisconne
         const user = this.jwtService.verify(token, {
           secret: this.configService.getOrThrow('JWT_ACCESS_SECRET'),
         });
-        logger.info(`Connected user id: ${user.id}`);
+        logger.info(`[SOCKET] Connected user id: ${user.id}`);
         client.join(user.id.toString());
       } catch (error) {
         console.log('err');
@@ -42,7 +42,7 @@ export class ConnectionGateway implements OnGatewayConnection, OnGatewayDisconne
         const user = this.jwtService.verify(token, {
           secret: this.configService.getOrThrow('JWT_ACCESS_SECRET'),
         });
-        logger.info(`Disconnected user id: ${user.id}`);
+        logger.info(`[SOCKET] Disconnected user id: ${user.id}`);
         client.leave(user.id.toString());
       } catch (error) {
         console.log('err');
