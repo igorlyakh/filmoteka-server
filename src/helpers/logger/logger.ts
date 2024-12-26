7ab19962672b2db8589e739e823782115045e7f8
Igor Liakh <igorlyah060@gmail.com>
2024-12-26 11:08:35 +0200

    added error logger

--- src/helpers/logger/logger.ts
+++ src/helpers/logger/logger.ts
@@ -13,4 +13,33 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
+logger.add(
+  new winston.transports.Console({
+    level: 'error',
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD, HH:mm:ss',
+      }),
+      winston.format.printf(info => {
+        return `\n[SERVER ERROR] - ${info.timestamp}     ${info.message}`.red;
+      })
+    ),
+  })
+);
+
+logger.add(
+  new winston.transports.File({
+    filename: 'error.log',
+    level: 'error',
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: 'YYYY-MM-DD, HH:mm:ss',
+      }),
+      winston.format.printf(info => {
+        return `[SERVER ERROR] - ${info.timestamp}     ${info.message}`;
+      })
+    ),
+  })
+);
+
 export default logger;
